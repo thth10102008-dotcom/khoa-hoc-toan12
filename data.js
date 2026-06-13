debc797293f8b0bc936f8fa83e434a1f16d8e711
thth10102008-dotcom <thth10102008@gmail.com>
2026-06-13 21:51:37 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,135 @@
+const courses = [
+
+{
+chapter:"CHƯƠNG I. ỨNG DỤNG ĐẠO HÀM",
+
+lessons:[
+
+{
+title:"Bài 1. Xét tính đơn điệu",
+
+video:"https://youtube.com",
+
+tailieu:"https://drive.google.com",
+
+btvn:"https://drive.google.com",
+
+chua:"https://thaychi.net"
+},
+
+{
+title:"Bài 2. Cực trị hàm số",
+
+video:"https://youtube.com",
+
+tailieu:"https://drive.google.com",
+
+btvn:"https://drive.google.com",
+
+chua:"https://thaychi.net"
+}
+
+]
+},
+
+{
+chapter:"CHƯƠNG II. VECTƠ VÀ HỆ TỌA ĐỘ",
+
+lessons:[
+
+{
+title:"Các phép toán vectơ",
+
+video:"https://youtube.com",
+
+tailieu:"https://drive.google.com",
+
+btvn:"#",
+
+chua:"#"
+}
+
+]
+},
+
+{
+chapter:"CHƯƠNG III. THỐNG KÊ",
+
+lessons:[
+
+{
+title:"Khoảng tứ phân vị",
+
+video:"https://youtube.com",
+
+tailieu:"https://drive.google.com",
+
+btvn:"#",
+
+chua:"#"
+}
+
+]
+},
+
+{
+chapter:"CHƯƠNG IV. NGUYÊN HÀM - TÍCH PHÂN",
+
+lessons:[
+
+{
+title:"Nguyên hàm cơ bản",
+
+video:"https://youtube.com",
+
+tailieu:"https://drive.google.com",
+
+btvn:"#",
+
+chua:"#"
+}
+
+]
+},
+
+{
+chapter:"CHƯƠNG V. OXYZ",
+
+lessons:[
+
+{
+title:"Phương trình mặt phẳng",
+
+video:"https://youtube.com",
+
+tailieu:"https://drive.google.com",
+
+btvn:"#",
+
+chua:"#"
+}
+
+]
+},
+
+{
+chapter:"CHƯƠNG VI. XÁC SUẤT CÓ ĐIỀU KIỆN",
+
+lessons:[
+
+{
+title:"Công thức xác suất có điều kiện",
+
+video:"https://youtube.com",
+
+tailieu:"https://drive.google.com",
+
+btvn:"#",
+
+chua:"#"
+}
+
+]
+}
+
+];
