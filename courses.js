0a37bdc3ad1b9a8f1f0438aa84c32e78ba9ea157
thth10102008-dotcom <thth10102008@gmail.com>
2026-06-13 21:35:29 +0700

    Create courses.js

---
+++ courses.js
@@ -0,0 +1,35 @@
+const courses = [
+
+{
+title:"CHƯƠNG I. ỨNG DỤNG ĐẠO HÀM",
+
+lessons:[
+
+{
+name:"Bài 1. Tính đơn điệu hàm số",
+
+video:"https://www.youtube.com/watch?v=tYQ-jRLEBJg",
+
+document:"https://drive.google.com/file/d/1AE1WTLNeiVkCWOo50A2uvl3_NCxqJwG3/view"
+},
+
+{
+name:"Bài 2. Cực trị hàm số",
+
+video:"https://www.youtube.com/watch?v=SMPfLiCQVms",
+
+document:"https://drive.google.com/file/d/1AGNYA9e9qcL2yx_KT6Y7Ydl6Oprtrhh8/view"
+},
+
+{
+name:"Bài 3. GTLN - GTNN",
+
+video:"https://www.youtube.com/watch?v=3yI4qHpDBis",
+
+document:"https://drive.google.com/file/d/1CKcVgniZ8cE-iq6Vky7wClZh2yP0gqf4/view"
+}
+
+]
+}
+
+];
